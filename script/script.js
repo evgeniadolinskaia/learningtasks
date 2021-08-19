e18e28b98fb6054f87d491b65b1be5a4db552116
Evgeniia Dolinskaia <edolinskaia@mail.com>
2021-08-19 21:53:20 +0300

    script fix

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function main () {
+	let userAnswer = prompt ("Введите целое число");
+	a=userAnswer;
+	userAnswer = prompt ("Введите целое число");
+	b=userAnswer;
+	let c = + a * + b % (+ a + + b);
+	alert (c)
+
+}
